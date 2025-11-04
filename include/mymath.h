8a2dbd29ff0327e385c420e74a521761beef156f
haechan7 <maskgocls@gmail.com>
2025-11-04 16:36:51 +0900

    Update: mymath.h

--- include/mymath.h
+++ include/mymath.h
@@ -1,7 +1,7 @@
 #pragma once
 class myMath {
 public:
-	double myPow(double base, int exp);
-	double myFabs(double x);
-	double mySqrt(double x);
+	static double myPow(double base, int exp);
+	static double myFabs(double x);
+	static double mySqrt(double x);
 };
